e1db37a4d9e7a5f6f18b000cab0ec342d8a03b8c
shuntian <978987373@qq.com>
2018-05-13 12:31:10 +0800

    完成首页布局的设计
    Signed-off-by: shuntian <978987373@qq.com>

---
+++ app.js
@@ -0,0 +1,96 @@
+var koa = require('koa');
+var controller = require('koa-route');
+var app = koa();
+var views = require('co-views');
+var querystring = require('querystring');
+var render = views('./view',{
+  map : {html : 'ejs'}
+});
+var koa_static = require('koa-static-server');
+var service = require('./service/webAppService.js');
+app.use(koa_static({
+  rootDir: './static/',
+  rootPath: '/static/',
+  maxage: 0
+}))
+
+app.use(controller.get('/route_test', function*(){
+  this.set('Cache-Control', 'no-cache');
+  this.body = "hello world";
+}))
+
+app.use(controller.get('/ejs_test', function*(){
+  this.set('Cache-Control', 'no-cache');
+  this.body = yield render('test',{title:'title_test'});
+}))
+
+app.use(controller.get('/api_test', function*(){
+  this.set('Cache-Control', 'no-cache');
+  var content = service.get_test_data();
+  this.body = service.get_test_data();
+}))
+
+app.use(controller.get('/', function*(){
+  this.set('Cache-Control', 'no-cache');
+  this.body = yield render('index',{title:'书城首页'});
+}))
+
+app.use(controller.get('/search', function*(){
+  this.set('Cache-Control', 'no-cache');
+  this.body = yield render('search',{title:'搜索页面'});
+}))
+
+
+app.use(controller.get('/book', function*(){
+  this.set('Cache-Control', 'no-cache');
+  var params = querystring.parse(this.req._parsedUrl.query);
+  var bookId = params.id;
+  this.body = yield render('book',{bookId:bookId});
+}))
+
+app.use(controller.get('/ajax/index', function*(){
+	this.set('Cache-Control', 'no-cache');
+	this.body = service.get_index_data();
+}))
+
+app.use(controller.get('/ajax/rank', function*(){
+	this.set('Cache-Control', 'no-cache');
+	this.body = service.get_rank_data();
+}))
+
+app.use(controller.get('/ajax/female', function*(){
+	this.set('Cache-Control', 'no-cache');
+	this.body = service.get_female_data();
+}))
+
+app.use(controller.get('/ajax/male', function*(){
+	this.set('Cache-Control', 'no-cache');
+	this.body = service.get_male_data();
+}))
+
+app.use(controller.get('/ajax/category', function*(){
+	this.set('Cache-Control', 'no-cache');
+	this.body = service.get_category_data();
+}))
+
+app.use(controller.get('/ajax/bookbcaket', function*(){
+	this.set('Cache-Control', 'no-cache');
+	this.body = service.get_bookcaket_data();
+}))
+
+
+
+app.use(controller.get('/ajax/book', function*(){
+  this.set('Cache-Control', 'no-cache');
+  var params = querystring.parse(this.req._parsedUrl.query);
+  var id = params.id;
+  if(!id){
+    id = "";
+  }
+	this.body = service.get_book_data(id);
+}))
+
+
+
+app.listen(3000);
+console.log('koa server is started!!');
---
+++ service/webAppService.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+exports.get_test_data = function() {
+  var content = fs.readFileSync('./mock/test.json','utf-8');
+  return content;
+}
+
+exports.get_index_data = function(){
+  var content = fs.readFileSync('./mock/home.json','utf-8');
+  return content;
+}
+
+exports.get_female_data = function(){
+  var content = fs.readFileSync('./mock/channel/female.json','utf-8');
+  return content;
+}
+
+exports.get_male_data = function(){
+  var content = fs.readFileSync('./mock/channel/male.json','utf-8');
+  return content;
+}
+
+exports.get_rank_data = function(){
+  var content = fs.readFileSync('./mock/rank.json','utf-8');
+  return content;
+}
+
+exports.get_bookbaket_data = function(){
+  var content = fs.readFileSync('./mock/bookbaket.json','utf-8');
+  return content;
+}
+
+exports.get_category_data = function(){
+  var content = fs.readFileSync('./mock/category.json','utf-8');
+  return content;
+}
+
+exports.get_book_data = function(id){
+  if(!id){
+    id = '18218';  
+  }
+  var content = fs.readFileSync('./mock/book/'+id+'.json','utf-8');
+  return content;
+}
